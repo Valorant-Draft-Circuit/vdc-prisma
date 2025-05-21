26af9f0c50e1bfb49139eaaf04121939b3f99faa
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2025-05-20 20:36:23 -0500

    4.0.1

---
+++ migrations/20250521013509_4_0_1/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - The values [MID_PLAYOFF,END_PLAYOFF] on the enum `Matches_matchType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE `Matches` MODIFY `matchType` ENUM('PRE_SEASON', 'BO2', 'BO3', 'BO5') NOT NULL;
