b90661443bc0e9acb2d4037c40d3fc15e7e515b2
Sooho <95513590+dlatngh@users.noreply.github.com>
2026-07-05 05:02:29 -0700

    add bracket pickems table (#30)

--- enums/_controlpanel.ts
+++ enums/_controlpanel.ts
@@ -31,4 +31,5 @@ export enum ControlPanelID {
     PICKEM_ADVANCE_LOCK             = 43,
     PICKEM_PREVIEW                  = 44,
     PICKEM_ENABLED                  = 45,
+    PICKEM_BRACKET_LOCK             = 46,
 }
